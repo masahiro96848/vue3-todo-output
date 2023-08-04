79051373b8c5780b3060c8309b6a929fc6098d74
masahiro96848 <feetwcpic9436@gmail.com>
2023-08-04 16:35:39 +0900

    Todoを追加する機能を実装

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,14 @@
             <h1 class="text-center">Todo List</h1>
             <v-card class="pa-10">
               <v-form>
-                <v-text-field variant="outlined"></v-text-field>
+                <v-text-field
+                  v-model="title"
+                  label="Todoを追加"
+                  class="mb-4"
+                  variant="outlined"
+                  @keyup.enter="handleSubmit"
+                ></v-text-field>
+                <v-text-field label="キーワード検索" variant="outlined"></v-text-field>
                 <v-list>
                   <v-list-item
                     class="mb-6"
@@ -15,10 +22,10 @@
                     height="60"
                     lines="two"
                     rounded
-                    v-for="todo in todos"
-                    :key="todo"
+                    v-for="item in todoList"
+                    :key="item.id"
                   >
-                    {{ todo }}
+                    {{ item.title }}
                   </v-list-item>
                 </v-list>
               </v-form>
@@ -31,13 +38,38 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, reactive } from 'vue'
+
+interface Todo {
+  id: number
+  title: string
+}
+
 export default defineComponent({
   setup() {
-    const todos = ref(['テスト1', 'テスト2', 'テスト3'])
+    const title = ref('')
+    const nextTodoId = ref(4)
+    const todoList: Todo[] = reactive([
+      { id: 1, title: 'テスト1' },
+      { id: 2, title: 'テスト2' },
+      { id: 3, title: 'テスト3' }
+    ])
+
+    const handleSubmit = () => {
+      if (title.value === '') return
+      const newTodo: Todo = {
+        id: nextTodoId.value,
+        title: title.value.trim() // 入力値をトリムしてから使用
+      }
+      todoList.push(newTodo) // 新しいTodoをtodosに追加
+      title.value = '' // 入力フィールドをクリア
+      nextTodoId.value++ // 次のTodoのIDを更新
+    }
 
     return {
-      todos
+      title,
+      todoList,
+      handleSubmit
     }
   }
 })
